e366dab2934dd9259a5e6ac7c13eeff9b1923fc6
JustBeYou <mihailferaru2000@gmail.com>
2018-06-28 13:05:45 +0300

    Add throw outside try

--- src/tests/main.c
+++ src/tests/main.c
@@ -3,11 +3,15 @@
 #include "exceptions.h"
 
 int main() {
+    exceptions_init();
+    
     try( 
     {
-      printf("Empty try\n");  
+      printf("Empty try\n");
     }
     );
+
+    throw(123);
 
     printf("test\n");
 
