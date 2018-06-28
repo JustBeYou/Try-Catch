bffc7941c037742c10b00517d9387e9a06ea67f6
JustBeYou <mihailferaru2000@gmail.com>
2018-06-28 11:44:57 +0300

    Description of try statement

--- src/try-catch/include/exceptions.h
+++ src/try-catch/include/exceptions.h
@@ -1,4 +1,44 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
+#include <stdbool.h>
+
+/*
+ * Try statement is used to execute a block of instructions until an exception
+ * is raisen. The block could not contain any catch or finnaly statement,
+ * the default behaviour is to end the block and ignore the exception.
+ * In that case there is no guarantee that there won't be any runtime error.
+ * Instruction block should be surrounded by brackets.
+ * Add catch/finally statements only at the end of the block,
+ * don't mix up things!
+ * Example
+ 
+ try( {
+        int x = 123;
+        int y = some_call(x);
+
+        printf ("%d\n", y);
+        
+        catch ( SOME_EXCEPTION, {
+            handle_some_call();
+        } );
+        finally ( {
+            printf ("DONE\n");
+        } );
+    }
+ );
+
+ */
+#define try(block) do { block } while (false) 
+
+/*
+ *
+ */
+#define catch(exception, block)
+
+/*
+ * 
+ */
+#define finally(block)
+
 #endif // EXCEPTIONS_H
