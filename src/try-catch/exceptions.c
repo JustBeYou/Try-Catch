940e7b577fd6b623e3c334325866a445cc217221
JustBeYou <mihailferaru2000@gmail.com>
2018-06-28 13:06:34 +0300

    Exceptions initialization and general handler support

--- src/try-catch/exceptions.c
+++ src/try-catch/exceptions.c
@@ -1,26 +1,41 @@
-#include <setjmp.h>
-#include <stdint.h>
-#include <stddef.h>
-
 #include "exceptions.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // this will become dynamic in future versions
-jmp_buf g_jmpBufferStack[DEFAULT_JMP_BUFFER_STACK_SIZE];
+// index 0 is reserved for unhandled exceptions outside try statements
+jmp_buf g_jmpBufStack[DEFAULT_JMP_BUFFER_STACK_SIZE];
 size_t  g_jmpCounter;
 
 void exceptions_init() {
-
+   int unhandledThrow = setjmp(g_jmpBufStack[0]);
+   if (unhandledThrow) {
+        exceptions_defaultHandler(unhandledThrow);
+   }
 }
 
-jmp_buf *exceptions_getCurrentJmpBuffer() {
-    return &g_jmpBufferStack[g_jmpCounter];
+jmp_buf *exceptions_getCurrentJmpBuf() {
+    return &g_jmpBufStack[g_jmpCounter];
 }
 
-jmp_buf *exceptions_getNewJmpBuffer() {
+jmp_buf *exceptions_getNewJmpBuf() {
     ++g_jmpCounter;
-    return exceptions_getCurrentJmpBuffer();
+    return exceptions_getCurrentJmpBuf();
 }
 
-void exceptions_destroyJmpBuffer() {
+void exceptions_destroyJmpBuf() {
     --g_jmpCounter;
+}
+
+size_t exceptions_getCurrentCounter() {
+    return g_jmpCounter;
+}
+
+jmp_buf *exceptions_getJmpBufAt(size_t p_index) {
+    return &g_jmpBufStack[p_index];
+}
+
+void exceptions_defaultHandler(int p_exception) {
+    printf("[EXCEPTION] Unhandled %d\n", p_exception);
+    exit(p_exception);
 }
