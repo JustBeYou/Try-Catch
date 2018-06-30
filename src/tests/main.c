366fbf567dd9b2a9e8efa15b624551ea704efcf3
JustBeYou <mihailferaru2000@gmail.com>
2018-06-30 11:56:22 +0300

    Generate some random exceptions to see how it works

--- src/tests/main.c
+++ src/tests/main.c
@@ -1,30 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "stdexcept.h"
 
-DECL_EXCEPTION(TestException);
+DECL_EXCEPTION(NullPointer);
+DECL_EXCEPTION(ValueError);
+DECL_EXCEPTION(GenericError);
 
 int main() {
     exceptions_init();
     
     try( 
-    {
-      printf("The try starts here\n");
-
-      throw(TestException("This is a test", NULL));
-
-      printf("Should not arrive here...\n");
-
-    }
-
-    catch(TestException(NULL, NULL),
         {
-            printf("Exception catched\n");
-        })
+            // TODO:this should not be called manually (BUG)
+            catch_registerException(NullPointer(NULL, NULL));
+            catch_registerException(ValueError(NULL, NULL));
+            catch_registerException(GenericError(NULL, NULL));
+            
+            srand(time(NULL));
+            int val = rand() % 100;
+            char *s = NULL;
 
-    )
+            if (val > 70) {
+                throw(ValueError("The generated value is too big", NULL));
+            } else if (val % 2 == 0){
+                s = malloc(512);
+            }
 
-    printf("Neither here\n");
+            if (s != NULL) free(s);
+            else throw(NullPointer("The pointer that needs to be freed is NULL", NULL));
+
+            if (val < 20) {
+                throw(GenericError("I just came here to blow your mind", NULL));
+            }
+        }
+
+        catch( NullPointer(NULL, NULL),
+            {
+                Exception *e = catch_getThrownException();
+                printf("We should alloc some memory (%s|%s)\n", e->name, e->msg);
+            }
+        )
+
+        catch( ValueError(NULL, NULL), 
+            {
+                Exception *e = catch_getThrownException();
+                printf("Hoooooold on (%s|%s)\n", e->name, e->msg);
+            }
+        )
+
+        catch( GenericError(NULL, NULL),
+            {
+                printf("This just does not matter\n");
+            }
+        )
+    );
+
+    throw(GenericError("DONE!", NULL));
 
     return 0;
 }
